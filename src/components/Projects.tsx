6747c07539cacd47e76a9480283084742fbd4f9d
Ankit Rajput <ankitrajput88073@gmail.com>
2025-10-14 16:35:36 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -196,11 +196,10 @@ const Projects: React.FC = () => {
             
            
             <a
-      href="https://github.com/your-ankit6392-git" 
+      href="https://github.com/ankit6392-git" 
       target="_blank"
       rel="noopener noreferrer"
-      className="flex items-center px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-gray-900 transition-colors"
-    >
+  className="flex items-center text-blue-600 hover:underline"    >
       <FiGithub className="w-5 h-5 mr-2" />
        View All Projects on GitHub
     </a>
